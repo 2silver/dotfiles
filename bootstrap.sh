9492ba68aba75ba9ecc8bbf5227b87d0c283961e
Michael Graf <michael.graf@fhstp.ac.at>
2018-03-07 11:45:44 +0100

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 ST_USERPATH="$HOME/Library/Application Support/Sublime Text 3/Packages/User";
 ST_SETTINGSPATH="$ST_USERPATH/Preferences.sublime-settings"
 ST_PACKAGESPATH="$ST_USERPATH/Package Control.sublime-settings"
-ST_PREFERENCESEPATH="HOME/.dotfiles/init/sublimetext/Preferences.sublime-settings"
+ST_SETTINGSDOTFILEPATH="$HOME/.dotfiles/init/sublimetext/Preferences.sublime-settings"
+ST_PACKAGESDOTFILEPATH="$HOME/.dotfiles/init/sublimetext/Package Control.sublime-settings"
 
 cd "$(dirname "${BASH_SOURCE}")";
 
@@ -35,39 +36,42 @@ function doIt() {
   source .cask
 
   # install oh-my-zsh
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
-  cp ./.zshrc ~/.zshrc;
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  cp ./.zshrc ~/.zshrc
 
   # Sublime Text
   # Make settings folder if it doesn't exist
   if [[ ! -d "$ST_USERPATH" ]]; then
-    echo "[sublimetext] Making settings folder.. ($ST_USERPATH)"
+    echo "[sublimetext settings] Making settings folder.. ($ST_USERPATH)"
     mkdir -p "$ST_USERPATH"
   fi
   # If file exists already, move into dotfiles (git will track differences)
   if [[ -f "$ST_SETTINGSPATH" ]] && [[ ! -h "$ST_SETTINGSPATH" ]]; then
-    echo "[sublimetext] User settings found, moving into dotfiles.. ($ST_PREFERENCESEPATH)"
-    mv "$ST_SETTINGSPATH" "$ST_PREFERENCESEPATH"
+    echo "[sublimetext settings] User settings found, moving into dotfiles.. ($ST_SETTINGSDOTFILEPATH)"
+    mv "$ST_SETTINGSPATH" "$ST_SETTINGSDOTFILEPATH"
   fi
   
   if [[ ! -L "$ST_SETTINGSPATH" ]]; then
-    echo "[sublimetext] Symlinking settings path to dotfiles.. ($ST_SETTINGSPATH)"
-    ln -s "$ST_PREFERENCESEPATH" "$ST_SETTINGSPATH"
+    echo "[sublimetext settings] Symlinking settings path to dotfiles.. ($ST_SETTINGSPATH)"
+    ln -s "$ST_SETTINGSDOTFILEPATH" "$ST_SETTINGSPATH"
   else
-    echo "[sublimetext] Everything looks good here. Nothing to setup."
+    echo "[sublimetext settings] Everything looks good here. Nothing to setup."
   fi
 
+
+
+
   # If file exists already, move into dotfiles (git will track differences)
-  if [[ -f "$ST_SETTINGSPATH" ]] && [[ ! -h "$ST_SETTINGSPATH" ]]; then
-    echo "[sublimetext] User packages found, moving into dotfiles.. ($ST_PACKAGESPATH)"
-    mv "$ST_SETTINGSPATH" "$ST_PACKAGESPATH"
+  if [[ -f "$ST_PACKAGESPATH" ]] && [[ ! -h "$ST_PACKAGESPATH" ]]; then
+    echo "[sublimetext packages] User packages found, moving into dotfiles.. ($ST_PACKAGESPATH)"
+    mv "$ST_PACKAGESPATH" "$ST_PACKAGESDOTFILEPATH"
   fi
   
-  if [[ ! -L "$ST_SETTINGSPATH" ]]; then
-    echo "[sublimetext] Symlinking packages path to dotfiles.. ($ST_SETTINGSPATH)"
-    ln -s "$ST_PACKAGESPATH" "$ST_SETTINGSPATH"
+  if [[ ! -L "$ST_PACKAGESPATH" ]]; then
+    echo "[sublimetext packages] Symlinking packages path to dotfiles.. ($ST_PACKAGESPATH)"
+    ln -s "$ST_PACKAGESDOTFILEPATH" "$ST_PACKAGESPATH"
   else
-    echo "[sublimetext] Everything looks good here. Nothing to setup."
+    echo "[sublimetext packages] Everything looks good here. Nothing to setup."
   fi
 
   ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
@@ -83,7 +87,6 @@ else
     doIt;
     say Automatische Installation abgeschlossen!;
     say Nicht vergessen, Pakete installieren;
-    open ./packages.md
   fi;
 fi;
 
