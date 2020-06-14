997ae3a9c5e40678b55eafb90d1d4e2b2b9125af
Michael Graf <michael.graf@fhstp.ac.at>
2020-06-14 17:16:53 +0200

    fix: update brew.sh install

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,6 +38,7 @@ main(){
 
   # Install HomeBrew
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
   # install brew + cask packages
   brew install git
   # we can now fetch out .dotfiles from repository
