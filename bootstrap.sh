1d68fafed2aec12c3a4d10d836eb22cdff32743e
Michael Graf <michael.graf@fhstp.ac.at>
2020-09-16 11:00:44 +0200

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,43 +1,78 @@
 #!/usr/bin/env bash
 
-# thanks to brew for sh code parts !!
+# Only enable exit-on-error after the non-critical colorization stuff,
+# which may fail on systems lacking tput or terminfo
+set -e
+
+# setup Variables
+VSCODE_USERPATH="$HOME/Library/Application\ Support/Code/User";
+VSCODE_DOTFILEPATH="$HOME/.dotfiles/init/vscode/"
+
+# Use colors, but only if connected to a terminal, and that terminal supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
+
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+append_to_zshrc() {
+  local text="$1" zshrc
+  local skip_new_line="${2:-0}"
+
+  if [ -w "$HOME/.zshrc.local" ]; then
+    zshrc="$HOME/.zshrc.local"
+  else
+    zshrc="$HOME/.zshrc"
+  fi
+
+  if ! grep -Fqs "$text" "$zshrc"; then
+    if [ "$skip_new_line" -eq 1 ]; then
+      printf "%s\n" "$text" >> "$zshrc"
+    else
+      printf "\n%s\n" "$text" >> "$zshrc"
+    fi
+  fi
+}
 
 main(){
-  # Use colors, but only if connected to a terminal, and that terminal
-  # supports them.
-  if which tput >/dev/null 2>&1; then
-      ncolors=$(tput colors)
-  fi
-  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-    RED="$(tput setaf 1)"
-    GREEN="$(tput setaf 2)"
-    YELLOW="$(tput setaf 3)"
-    BLUE="$(tput setaf 4)"
-    BOLD="$(tput bold)"
-    NORMAL="$(tput sgr0)"
-  else
-    RED=""
-    GREEN=""
-    YELLOW=""
-    BLUE=""
-    BOLD=""
-    NORMAL=""
-  fi
-
-  # Only enable exit-on-error after the non-critical colorization stuff,
-  # which may fail on systems lacking tput or terminfo
-  set -e
-
-  # setup Variables
-  VSCODE_USERPATH="$HOME/Library/Application\ Support/Code/User";
-  VSCODE_DOTFILEPATH="$HOME/.dotfiles/init/vscode/"
 
   if [ ! -n "$DIR" ]; then
     DIR=~/.dotfiles
   fi
 
   # Install HomeBrew
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  if ! command -v brew >/dev/null; then
+    fancy_echo "Installing Homebrew ..."
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+
+    append_to_zshrc '# recommended by brew doctor'
+
+    # shellcheck disable=SC2016
+    append_to_zshrc 'export PATH="/usr/local/bin:$PATH"' 1
+
+    export PATH="/usr/local/bin:$PATH"
+  fi
+
 
   # install brew + cask packages
   brew install git
@@ -56,9 +91,10 @@ main(){
   # setup all configs ...
   # Load bash profile
   source ~/.bash_profile;
-  ## nodejs / nvm
+
   # install nvm -> does not work with brew
   curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh |  bash
+
   ## rvm
   curl -sSL https://get.rvm.io | bash -s stable
 
@@ -74,12 +110,12 @@ main(){
   # Microsoft Visual Code
   # Make settings folder if it doesn't exist
   if [[ ! -d "$VSCODE_USERPATH" ]]; then
-    echo "[vscode settings] Making settings folder.. ($ST_USERPATH)"
+    fancy_echo "[vscode settings] Making settings folder.. ($ST_USERPATH)"
     ln -sfn "$VSCODE_DOTFILEPATH"/* "$VSCODE_USERPATH"
   fi
   # If file exists already, move into dotfiles (git will track differences)
   if [[ -f "$VSCODE_USERPATH" ]] && [[ ! -h "$VSCODE_USERPATH" ]]; then
-    echo "[vscode settings] User settings found, moving into dotfiles.. ($VSCODE_DOTFILEPATH)"
+    fancy_echo "[vscode settings] User settings found, moving into dotfiles.. ($VSCODE_DOTFILEPATH)"
     rm -rf "$VSCODE_USERPATH"
     ln -sfn "$VSCODE_DOTFILEPATH"/* "$VSCODE_USERPATH"
   fi
@@ -90,9 +126,7 @@ main(){
   # ln -s /Users/nscgraf/.dotfiles/init/vscode/snippets/ /Users/nscgraf/Library/Application\ Support/Code/User
   # END
 
-
-
-  printf "${GREEN}"
+  fancy_echo "${GREEN}"
   echo ''
   echo '     _       _    __ _ _           '
   echo '    | |     | |  / _(_) |          '
@@ -105,14 +139,13 @@ main(){
   echo 'Setup complete, thank you'
   echo ''
   echo ''
-  printf "${NORMAL}"
+  fancy_echo "${NORMAL}"
 }
 
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   main;
-  say Automatische Installation abgeschlossen!;
-  say Nicht vergessen, Pakete installieren;
+  say Installation finished;
 fi;
 unset main;
